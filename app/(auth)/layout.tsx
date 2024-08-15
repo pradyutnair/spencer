ff4df6447308271659e60d4228f045cfa283abe5
Pradyut Nair <pradyutnair.work@gmail.com>
2024-08-15 21:25:23 +0200

    Updated ui

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,20 +8,21 @@ export default function RootLayout({
 }>) {
   return (
     <main className="flex w-full h-screen justify-between font-inter relative overflow-hidden">
-      <div className="absolute inset-0 z-0 hidden lg:block">
+      <div className="absolute inset-0 z-0">
         <Image
           src="/auth-image.jpg"
           alt="Background"
           layout="fill"
           objectFit="cover"
           quality={100}
+          className="w-full h-full object-cover"
         />
       </div>
-      <div className="z-10 ml-4 mr-4 w-full items-center justify-center">
+      <div className="z-10 ml-4 mr-4 w-full flex items-center justify-center">
         {children}
       </div>
       <div className="relative z-10 hidden lg:block">
-        <div className="mt-72 ml-32 justify-center transform scale-150 rounded-lg opacity-85 halo">
+        <div className="mt-52 ml-32 justify-center items-center transform scale-125 rounded-lg opacity-85 halo">
           <Image
             className="rounded-lg"
             src="/main-dashboard.png"
